d10974307bbd70604f43cf52cfba163c30f3366c
yanJuicy <hanjub3@gmail.com>
2023-05-26 03:51:16 +0900

    컨슈머 API

---
+++ simple-kafka-consumer/src/main/java/com/example/ConsumerWithASyncCommit.java
@@ -0,0 +1,54 @@
+package com.example;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Properties;
+
+public class ConsumerWithASyncCommit {
+
+    private final static Logger logger = LoggerFactory.getLogger(ConsumerWithASyncCommit.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVER = "";
+    private final static String GROUP_ID = "test-group";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
+        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+
+        while(true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("{}", record);
+            }
+            consumer.commitAsync(new OffsetCommitCallback() {
+                @Override
+                public void onComplete(Map<TopicPartition, OffsetAndMetadata> offsets, Exception exception) {
+                    if (exception != null) {
+                        logger.error("Commit failed");
+                    } else {
+                        logger.info("Commit succeeded");
+                    }
+
+                    if (exception != null) {
+                        logger.error("Commit failed for offsets {}", offsets, exception);
+                    }
+                }
+            });
+        }
+    }
+
+}
